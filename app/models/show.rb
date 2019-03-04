8ac9d7b6cfcd8e5f041b1159e075c30105c3293b
Richard Cho <richardcho100@gmail.com>
2019-03-04 18:45:56 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,11 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.maximum(:rating)
   end
+
+  def self.most_popular_show
+    self.where(rating: self.highest_rating)
+  end
+
+
+  
 end
